0aed2fc9afbdfaea082fa36fed4417c4731852c3
Chloe Vilain <16856295+chloe-vilain@users.noreply.github.com>
2025-01-08 14:04:47 -0500

    feat: update the step flow for Android

--- step.sh
+++ step.sh
@@ -1,22 +1,47 @@
-#!/bin/bash
+#!/bin/sh
+
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+# Change to source directory
+cd $BITRISE_SOURCE_DIR
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+# Maestro version
+if [[ -z "$maestro_cli_version" ]]; then
+    echo "Maestro CLI version not specified, using latest"
+else
+    echo "Maestro CLI version: $maestro_cli_version"
+    export MAESTRO_VERSION=$maestro_cli_version;
+fi
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+# Install maestro CLI
+echo "Installing Maestro CLI"
+curl -Ls "https://get.maestro.mobile.dev" | bash
+export PATH="$PATH":"$HOME/.maestro/bin"
+echo "MAESTRO INSTALLED - Check Version"
+maestro -v
+
+# Run Maestro Cloud
+xcrun simctl install booted $app_file
+adb shell screenrecord /sdcard/ui_tests.mp4 &
+screenrecord_pid=$!
+maestro test $workspace/ --format junit --output $BITRISE_DEPLOY_DIR/test_report.xml $additional_params || true
+kill -SIGINT $screenrecord_pid || true
+adb pull /sdcard/ui_tests.mp4 $BITRISE_DEPLOY_DIR/ui_tests.mp4
+adb shell rm /sdcard/ui_tests.mp4
+
+
+
+# Export test results
+# Test report file
+[[ "$export_test_report" == "true" ]] && is_export="true"
+if [[ "$is_export" == "true" ]]; then
+    test_run_dir="$BITRISE_TEST_RESULT_DIR/maestro"
+    mkdir -p "$test_run_dir"
+    cp $BITRISE_DEPLOY_DIR/test_report.xml "$test_run_dir/maestro_report.xml"
+    if [[ -f "$BITRISE_DEPLOY_DIR/ui_tests.mp4" ]]; then
+        cp $BITRISE_DEPLOY_DIR/ui_tests.mp4 "$test_run_dir/ui_tests.mp4"
+    else
+        echo "Video file not found: $BITRISE_DEPLOY_DIR/ui_tests.mp4"
+    fi
+    echo '{"maestro-test-report":"Maestro Android Flows"}' >> "$test_run_dir/test-info.json"
+fi
