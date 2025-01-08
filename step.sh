1b12b92185372c0c77336ac9bd5680b597170b1e
Chloe Vilain <16856295+chloe-vilain@users.noreply.github.com>
2025-01-08 16:06:32 -0500

    fix: remove ios code

--- step.sh
+++ step.sh
@@ -21,7 +21,6 @@ echo "MAESTRO INSTALLED - Check Version"
 maestro -v
 
 # Run Maestro Cloud
-xcrun simctl install booted $app_file
 adb shell screenrecord /sdcard/ui_tests.mp4 &
 screenrecord_pid=$!
 maestro test $workspace/ --format junit --output $BITRISE_DEPLOY_DIR/test_report.xml $additional_params || true
